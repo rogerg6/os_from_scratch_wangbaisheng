595aee9ce892d3401471370a9c2814f0b675d026
rogerg6 <rogerg6@163.com>
2025-03-26 18:51:25 +0800

    [modify] 局部变量

--- ch04/sum.s
+++ ch04/sum.s
@@ -5,17 +5,18 @@
 sum:
     push %bp                # 保存主调函数bp
     mov %sp, %bp            # 赋值被调函数bp为当前sp
-    sub $8, %sp             # sp -= 8
+    sub $2, %sp             # sp -= 2, 局部变量，用来存储累加值。相当于int sum;
 
     mov $1, %bx
-    mov $0, %ax
+    movw $0, -2(%bp)        # 通过bp指针来寻址局部变量
 1:
-    add %bx, %ax
+    add %bx, -2(%bp)
     inc %bx
     #cmp 4(%bp), %bx         # 比较主调函数传入的参数
     cmp %di, %bx
     jbe 1b
 
+    mov -2(%bp), %ax
     mov %bp, %sp            # sp += 8, 释放被调函数栈空间
     pop %bp                 # 恢复主调函数bp
     ret
