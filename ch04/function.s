ac5a56b2fd60d7474fa0a58970acdaea5e47f474
rogerg6 <rogerg6@163.com>
2025-03-26 08:52:12 +0800

    [modify] 增加栈帧

--- ch04/function.s
+++ ch04/function.s
@@ -14,6 +14,10 @@ start:
     hlt
 
 sum:
+    push %bp                # 保存主调函数bp
+    mov %sp, %bp            # 赋值被调函数bp为当前sp
+    sub $8, %sp             # sp -= 8
+
     mov $1, %bx
     mov $0, %ax
 1:
@@ -21,6 +25,9 @@ sum:
     inc %bx
     cmp $10, %bx
     jbe 1b
+
+    mov %bp, %sp            # sp += 8, 释放被调函数栈空间
+    pop %bp                 # 恢复主调函数bp
     ret
 
 .org 0x1000
