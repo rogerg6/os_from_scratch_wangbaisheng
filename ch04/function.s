70881d1a05b5f1791c063e45e2d254ee58593da0
rogerg6 <rogerg6@163.com>
2025-03-26 08:58:10 +0800

    [modify] 多模块、符号可见性

--- ch04/function.s
+++ ch04/function.s
@@ -13,22 +13,5 @@ start:
     out %ax, %dx
     hlt
 
-sum:
-    push %bp                # 保存主调函数bp
-    mov %sp, %bp            # 赋值被调函数bp为当前sp
-    sub $8, %sp             # sp -= 8
-
-    mov $1, %bx
-    mov $0, %ax
-1:
-    add %bx, %ax
-    inc %bx
-    cmp $10, %bx
-    jbe 1b
-
-    mov %bp, %sp            # sp += 8, 释放被调函数栈空间
-    pop %bp                 # 恢复主调函数bp
-    ret
-
 .org 0x1000
 stack:
---
+++ ch04/sum.s
@@ -0,0 +1,20 @@
+.text
+.code16
+.global sum                 # 伪指令，告诉汇编器sum是全局可见符号
+
+sum:
+    push %bp                # 保存主调函数bp
+    mov %sp, %bp            # 赋值被调函数bp为当前sp
+    sub $8, %sp             # sp -= 8
+
+    mov $1, %bx
+    mov $0, %ax
+1:
+    add %bx, %ax
+    inc %bx
+    cmp $10, %bx
+    jbe 1b
+
+    mov %bp, %sp            # sp += 8, 释放被调函数栈空间
+    pop %bp                 # 恢复主调函数bp
+    ret
