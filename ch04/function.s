777adc27988bdbefbf5b6dfb4782c89e11a56c5a
rogerg6 <rogerg6@163.com>
2025-03-26 19:46:37 +0800

    [modify] 寄存器保存、恢复

--- ch04/function.s
+++ ch04/function.s
@@ -8,18 +8,56 @@
 #
 # 2. 寄存器传递
 #    效率比栈高。x86的64位模式下，依次用rdi, rsi, rdx, rcx, r8-9传递参数
+#
+# 返回值传递一般用AX
+#
+# 寄存器保存恢复
+#               主调保存                被调保存
+# 32bit        eax edx ecx           ebx edi esi ebp
+# 64bit    rdi rsi rdx rcx r8~11     rbx rbp r12~15
+# 
+# +--------------+
+# |主调保存寄存器 |
+# +--------------+    主调栈帧
+# |  被调函数参数 |
+# +--------------+----------------
+# |   返回地址    |
+# +--------------+    被调栈帧
+# |  主调函数BP   |
+# +--------------+ <--- BP
+# |   局部变量    |
+# +--------------+
+# |被调保存寄存器 |
+# +--------------+ <--- SP
 
 .text
 .code16
 
 start:
     mov $stack, %sp         # 初始化栈指针
+
+    # 调用sum前，给bx，cx赋值
+    # 调用sum后，打印bx，cx的值
+    # sum中使用了bx，所以需要调用前后保存、恢复bx
+    mov $0x41, %bx
+    mov $0x42, %cx
+
+    push %cx
+
     #push $50                # 用栈传递参数，sum(50)
     mov $50, %di            # 用di传递参数
     call sum
 
+    pop %cx
+
     mov $0x3f8, %dx
     out %ax, %dx
+
+    mov %bx, %ax
+    out %ax, %dx
+    mov %cx, %ax
+    out %ax, %dx
+    
     hlt
 
 .org 0x1000
--- ch04/sum.s
+++ ch04/sum.s
@@ -6,6 +6,8 @@ sum:
     push %bp                # 保存主调函数bp
     mov %sp, %bp            # 赋值被调函数bp为当前sp
     sub $2, %sp             # sp -= 2, 局部变量，用来存储累加值。相当于int sum;
+    
+    push %bx                # 保存寄存器
 
     mov $1, %bx
     movw $0, -2(%bp)        # 通过bp指针来寻址局部变量
@@ -17,6 +19,9 @@ sum:
     jbe 1b
 
     mov -2(%bp), %ax
+
+    pop %bx                 # 恢复寄存器
+
     mov %bp, %sp            # sp += 8, 释放被调函数栈空间
     pop %bp                 # 恢复主调函数bp
     ret
