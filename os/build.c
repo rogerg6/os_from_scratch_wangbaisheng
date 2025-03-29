60c78a8ca872391b3640c75d17f8d801f0e68b5d
rogerg6 <rogerg6@163.com>
2025-03-29 16:38:14 +0800

    [add] 32位引导过程（实模式->保护模式）

---
+++ os/build.c
@@ -0,0 +1,75 @@
+/**
+1. 内核映像(kernel.bin)布局
+
+          +------------+------------+
+ offset   |     0x0    |  0x10000   |
+          +------------+------------+
+ text     | boot16.bin | boot32.bin |
+          +------------+------------+
+
+2. 内核加载到内存的布局
+
+          +------------+------------+
+  PA      |   0x10000  |  0x20000   |
+          +------------+------------+
+ text     | boot16.bin | boot32.bin |
+          +------------+------------+
+
+*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// kvmtool默认加载到0x1000:0, 即物理地址0x10000处
+#define BOOT16_START_ADDR   0x10000         // boot16.bin加载物理地址
+#define BOOT32_START_ADDR   0x20000         // boot32.bin加载物理地址
+
+int main(void) {
+    int fd_out;
+    int fd;
+    char buf[512];
+    int c;
+
+    fd_out = open("kernel.bin", O_WRONLY | O_CREAT, 0664);
+    if (fd_out < 0) {
+        fprintf(stderr, "create kernel.bin failed.\n");
+        return -1;
+    }
+
+    // read boot16.bin
+    fd = open("boot16.bin", O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "open boot16.bin failed.\n");
+        return -2;
+    }
+    while (1) {
+        c = read(fd, buf, 512);
+        if (c > 0)
+            write(fd_out, buf, c);
+        else
+            break;
+    }
+    close(fd);
+
+    lseek(fd_out, BOOT32_START_ADDR - BOOT16_START_ADDR, SEEK_SET);
+
+    // read boot32.bin
+    fd = open("boot32.bin", O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "open boot32.bin failed.\n");
+        return -3;
+    }
+    while (1) {
+        c = read(fd, buf, 512);
+        if (c > 0)
+            write(fd_out, buf, c);
+        else
+            break;
+    }
+    close(fd);
+
+    close(fd_out);
+    return 0;
+}
