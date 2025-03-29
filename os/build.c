e58315346d8fca886d75b3a0fa12d8634d9f5bef
rogerg6 <rogerg6@163.com>
2025-03-29 20:30:22 +0800

    [add] 保护模式 -> 64位模式

--- os/build.c
+++ os/build.c
@@ -1,19 +1,19 @@
 /**
 1. 内核映像(kernel.bin)布局
 
-          +------------+------------+
- offset   |     0x0    |  0x10000   |
-          +------------+------------+
- text     | boot16.bin | boot32.bin |
-          +------------+------------+
+          +------------+------------+------------+
+ offset   |     0x0    |  0x10000   |  0xf0000   |
+          +------------+------------+------------+
+ text     | boot16.bin | boot32.bin | system.bin |
+          +------------+------------+------------+
 
 2. 内核加载到内存的布局
 
-          +------------+------------+
-  PA      |   0x10000  |  0x20000   |
-          +------------+------------+
- text     | boot16.bin | boot32.bin |
-          +------------+------------+
+          +------------+------------+------------+
+  PA      |   0x10000  |  0x20000   |  0x100000  |
+          +------------+------------+------------+
+ text     | boot16.bin | boot32.bin | system.bin |
+          +------------+------------+------------+
 
 */
 #include <stdio.h>
@@ -23,8 +23,9 @@
 #include <unistd.h>
 
 // kvmtool默认加载到0x1000:0, 即物理地址0x10000处
-#define BOOT16_START_ADDR   0x10000         // boot16.bin加载物理地址
-#define BOOT32_START_ADDR   0x20000         // boot32.bin加载物理地址
+#define BOOT16_START_ADDR   0x10000          // boot16.bin加载物理地址
+#define BOOT32_START_ADDR   0x20000          // boot32.bin加载物理地址
+#define SYSTEM_START_ADDR   0x100000         //system.bin加载物理地址
 
 int main(void) {
     int fd_out;
@@ -59,6 +60,23 @@ int main(void) {
     fd = open("boot32.bin", O_RDONLY);
     if (fd < 0) {
         fprintf(stderr, "open boot32.bin failed.\n");
+        return -3;
+    }
+    while (1) {
+        c = read(fd, buf, 512);
+        if (c > 0)
+            write(fd_out, buf, c);
+        else
+            break;
+    }
+    close(fd);
+
+    lseek(fd_out, SYSTEM_START_ADDR - BOOT16_START_ADDR, SEEK_SET);
+
+    // read system
+    fd = open("system.bin", O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "open system.bin failed.\n");
         return -3;
     }
     while (1) {
